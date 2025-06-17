60f7eabe8200fe7e757c15d7f2eef8b74673fc07
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-06-17 19:51:45 +0300

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <set>
+#include "merge.h"
+
 using namespace std;
 
 
@@ -140,23 +142,3 @@ void merge(const string &base, const string &currentBranch, const string &target
 }
 
 
-int main(int argc, char* argv[]){
-    if(argc != 3){
-        cout<<"Usage: merge <branch name>";
-        return 1;
-    }
-
-    string command = argv[1];
-    string targetBranch = argv[2];
-
-    if(command == "merge"){
-        string currentBranch = getCurrentBranch();
-        cout<<"current branch: "<< currentBranch;
-        string base = getLeastCommonAnscestor(currentBranch, targetBranch);
-        string commitA = getBranchCommit(currentBranch);
-        string  commitB = getBranchCommit(targetBranch);
-        merge(base, commitA, commitB);
-        cout<<"Merge completed. Merged file: .minigit/merged.txt";
-    }
-
-}
