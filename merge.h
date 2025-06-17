fba4b5ae26f1b0e63655e8bb521af45ba92db860
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-06-17 19:50:40 +0300

    add merge.h

---
+++ merge.h
@@ -0,0 +1,15 @@
+#ifndef MERGE_H
+#define MERGE_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+string getCurrentBranch();
+string getBranchCommit(const string& branchName);
+string getParentCommit(const string& commitId);
+string getLeastCommonAnscestor(const string& branch1, const string& branch2);
+vector<string> readFile(const string &path);
+void merge(const string &base, const string &currentBranch, const string &targetBranch);
+
+#endif
