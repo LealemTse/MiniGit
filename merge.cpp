51454350ea0be13a38c9ca9f38a5dcb8488f89b4
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-06-17 09:55:45 +0300

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -6,6 +6,56 @@
 #include <vector>
 using namespace std;
 
+string branch2;
+string currnetBranch;
+
+
+//gets the  current branch
+string getCurrentBranch(){
+    string path = ".minigit/HEAD";
+    ifstream headFile(path);
+    if(!headFile){
+        throw runtime_error("Could not open HEAD file.");
+    }
+
+    string line;
+    getline(headFile, line);
+
+    if (line.substr(0, 5) == "ref: refs/heads/") {
+        return line.substr(16);
+    }else{
+        return "";
+    }
+}
+
+//gets commitid in a branch
+
+string getBranchCommit(const string& branchName) {
+    string path = ".minigit/refs/heads/" + branchName;
+    ifstream branch(path);
+    if (!branch) {
+        throw runtime_error("branch not found");
+    }
+    string commit;
+    getline(branch, commit);
+    return commit;
+}
+
+
+//gets parentcommit
+
+string getParentCommit(const string& commitId) {
+    ifstream file(".minigit/objects/" + commitId);
+    string line;
+    while (getline(file, line)) {
+        if (line.substr(0, 7) == "parent ") {
+            return line.substr(7);
+        }
+    }
+    return "";
+}
+
+
 vector<string> readFile(const string &path)
 {
     ifstream file(path);
@@ -18,40 +68,40 @@ vector<string> readFile(const string &path)
     return lines;
 }
 
-void mergeFiles(const string &base, const string &branch1, const string &branch2)
+void mergeFiles(const string &base, const string &currentBranch, const string &branch2)
 {
     string basePath = ".minigit/objects/" + base;
-    string branch1Path = ".minigit/objects/" + branch1;
+    string currentBranchPath = ".minigit/objects/" + currentBranch;
     string branch2Path = ".minigit/objects/" + branch2;
 
     vector<string> baseLines = readFile(basePath);
-    vector<string> branch1Lines = readFile(branch1Path);
+    vector<string> currentBranchLines = readFile(currentBranchPath);
     vector<string> branch2Lines = readFile(branch2Path);
 
-    size_t maxSize = max(base.size(), max(branch1.size(), branch2.size()));
+    size_t maxSize = max(base.size(), max(currentBranch.size(), branch2.size()));
 
     vector<string> result;
 
     for (size_t i = 0; i < maxSize; ++i)
     {
-        if (base[i] == branch1[i] && base[i] == branch2[i])
+        if (base[i] == currentBranch[i] && base[i] == branch2[i])
         {
-            result.push_back(branch1[i]);
+            result.push_back(currentBranch[i]);
         }
 
-        else if (branch1[i] == branch2[i])
+        else if (currentBranch[i] == branch2[i])
         {
-            result.push_back(branch1[i]);
+            result.push_back(currentBranch[i]);
         }
 
-        else if (base[i] == branch1[i])
+        else if (base[i] == currentBranch[i])
         {
             result.push_back(branch2[i]);
         }
 
         else if (base[i] == branch2[i])
         {
-            result.push_back(branch1[i]);
+            result.push_back(currentBranch[i]);
         }
     }
 
@@ -62,4 +112,10 @@ void mergeFiles(const string &base, const string &branch1, const string &branch2
     {
         out << result[i] << endl;
     }
+}
+
+int main(){
+    Repository repo(".", false);
+    currentBranch = repo.getCurrentBranch();
+    cout << "Current branch: " << currentBranch << endl;
 }
