2bf29fb9625b8ba9aee07ab9c713bdca3c68b1a1
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-06-19 08:46:05 +0300

    Update merge.h

--- merge.h
+++ merge.h
@@ -11,5 +11,6 @@ string getParentCommit(const string& commitId);
 string getLeastCommonAnscestor(const string& branch1, const string& branch2);
 vector<string> readFile(const string &path);
 void merge(const string &base, const string &currentBranch, const string &targetBranch);
+void diff(const string &commit1, const string &commit2);
 
 #endif
