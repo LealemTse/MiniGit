72fd8c7bc6b9ae1326177c00d8d530f8ec74cda8
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-06-19 08:51:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    if(argc != 3){
-        cout<<"Usage: merge <branch name>";
-        return 1;
-    }
-
     string command = argv[1];
-    string targetBranch = argv[2];
-
     if(command == "merge"){
+        if(argc != 3){
+            cout<<"Usage: merge <branch name>";
+            return 1;
+        }
+        string targetBranch = argv[2];
         string currentBranch = getCurrentBranch();
         cout<<"current branch: "<< currentBranch;
         string base = getLeastCommonAnscestor(currentBranch, targetBranch);
@@ -20,5 +18,16 @@ int main(int argc, char* argv[]){
         merge(base, commitA, commitB);
         cout<<"Merge completed. Merged file: .minigit/merged.txt";
     }
+
+    if(command=="diff"){
+        if(argc!=4){
+            cout<<"Usage: diff <commit1> <commit2>";
+            return 1;
+        }
+        string commit1 = argv[2];
+        string commit2 = argv[3];
+        diff(commit1, commit2);
+    }
+    
 
 }
