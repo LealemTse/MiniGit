2e8d2c2f8c079d6139a3ccc1e5393085e1823fbc
bitaniyadamtew <bitaniyadamtew@gmail.com>
2025-06-19 12:18:23 +0300

    Add files via upload
    LAST

--- Repository.h
+++ Repository.h
@@ -2,17 +2,52 @@
 #define REPOSITORY_H
 
 #include <string>
+#include <vector>
 #include <unordered_map>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <filesystem>
 using namespace std;
-class Repository {
-public:
-  string worktree;
-  string gitdir;
-  unordered_map<string, string> config;
 
-  Repository(const string& path, bool force=false);
-  void init();
-  string repoFile(const string& relativePath);
-  void loadConfig(const string& configPath);
+class Commit {
+public:
+  string hash;
+  string message;
+  string author;
+  time_t timestamp;
+  vector<std::string> blobHashes;
+  Commit* parent;
+
+  Commit(string msg, string a, Commit* p = nullptr);
+  void generateHash();
 };
+string customHash(const string& filecontent);
+
+class Repository {
+private:
+  string gitDir;
+  string objectsDir;
+  unordered_map<string, string> blob;
+  unordered_map<string, string> config;
+  Commit* head = nullptr;
+
+  void initRepo();
+  void createDirectory(const string& path);
+  void loadConfig(const string& configPath);
+  void saveIndex();
+  void loadIndex();
+  void clearIndex();
+
+public:
+  Repository(string dir = ".minigit", bool force =false);
+  ~Repository();
+  void init();
+  std::string createBlob(const string& content);
+  void commit(const string& message, const std::string& author);
+  std::string getBlob(const string& hash);
+  void addFile(const string& filepath);
+};
+
 #endif
